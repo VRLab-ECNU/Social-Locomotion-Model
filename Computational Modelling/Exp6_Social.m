86786e49d42c7ac81cc65260d239a76b1c97814a
DBPR-Social-Robot <96282962+DBPR-Social-Robot@users.noreply.github.com>
2021-12-17 16:14:50 +0800

    Add files via upload

---
+++ Computational Modelling/Exp6_Social.m
@@ -0,0 +1,164 @@
+clc;clear;
+addpath('./functions');
+load('./data/fit_parameters.mat');
+m = x(1);
+n = x(2);
+a = 0.24;
+b = 0.12;
+c = x(3);
+th = x(4)*10;
+width = 10; % How wide is the map
+StartPoint = [1;500];
+EndPoint = [499;500];
+Target = [0,5]; % The position of the target pole
+BlockerL_Ori = 90; 
+BlockerR_Ori = 270;
+precision = 0.01; % How precise is the map
+RTsettings = struct('useFD',1,'stepSize',1);
+pathlengthL = [];pathlengthR = [];
+socialEnergyL = [];socialEnergyR = [];
+TimeL = [];TimeR = [];
+
+%% IF MUST LEFT
+for i = 1:5
+    LD = i*0.2 - 0.8;
+    for IPD = 2:4
+        socialImpact = 0;
+        BlockerL = [width/2 + LD , 2.5];
+        BlockerR = [width/2 + LD + IPD , 2.5];
+        EnergyMap1 = GetMap(width/precision+1,Target(2)/precision+1,BlockerL/precision,BlockerL_Ori,m,n,a,b,c,precision);
+        EnergyMap2 = GetMap(width/precision+1,Target(2)/precision+1,BlockerR/precision,BlockerR_Ori,m,n,a,b,c,precision);
+        EnergyMap = EnergyMap1 + EnergyMap2;
+        EnergyMap = rescale(EnergyMap,0,1,'InputMin',0,'InputMax',th);
+        % Key manipulation %
+        EnergyMap(round(BlockerL(1)/precision):round(BlockerR(1)/precision),251) = 1;
+        % Key manipulation %
+        SpeedMap = 1 - EnergyMap;
+        T = fm(SpeedMap,StartPoint,[1 1],struct('implementation','C++','order',2));
+        path = rayTrace(T,EndPoint,StartPoint,RTsettings);
+        path2 = sortrows(path',2);
+        path3 = interp1(path2(:,2),path2(:,1),1:470);
+        path3(1) = (path3(2)-500)/2+500;
+        h = diff(path3);
+        l = sum(sqrt(h.^2+1));
+        pathlengthL = [pathlengthL;[LD,IPD,l]];
+        for j = 1:length(path)-1
+            socialImpact = socialImpact + EnergyMap(round(path(1,j)),round(path(2,j)));
+        end
+        socialEnergyL = [socialEnergyL;[LD,IPD,socialImpact]];
+        TimeL = [TimeL;[LD,IPD,T(500,500)]];
+    end
+end
+
+%% IF MUST RIGHT
+for i = 1:5
+    LD = i*0.2 - 0.8;
+    for IPD = 2:4
+        socialImpact = 0;
+        BlockerL = [width/2 + LD , 2.5];
+        BlockerR = [width/2 + LD + IPD , 2.5];
+        EnergyMap1 = GetMap(width/precision+1,Target(2)/precision+1,BlockerL/precision,BlockerL_Ori,m,n,a,b,c,precision);
+        EnergyMap2 = GetMap(width/precision+1,Target(2)/precision+1,BlockerR/precision,BlockerR_Ori,m,n,a,b,c,precision);
+        EnergyMap = EnergyMap1 + EnergyMap2;
+        EnergyMap = rescale(EnergyMap,0,1,'InputMin',0,'InputMax',th);
+        % Key manipulation %
+        EnergyMap(1:round(BlockerL(1)/precision),251) = 1;
+        EnergyMap(round(BlockerR(1)/precision:1001),251) = 1;
+        % Key manipulation %
+        SpeedMap = 1 - EnergyMap;
+        T = fm(SpeedMap,StartPoint,[1 1],struct('implementation','C++','order',2));
+        path = rayTrace(T,EndPoint,StartPoint,RTsettings);
+        path2 = sortrows(path',2);
+        path3 = interp1(path2(:,2),path2(:,1),1:470);
+        path3(1) = (path3(2)-500)/2+500;
+        h = diff(path3);
+        l = sum(sqrt(h.^2+1));
+        pathlengthR = [pathlengthR;[LD,IPD,l]];
+        for j = 1:length(path)-1
+            socialImpact = socialImpact + EnergyMap(round(path(1,j)),round(path(2,j)));
+        end
+        socialEnergyR = [socialEnergyR;[LD,IPD,socialImpact]];
+        TimeR = [TimeR;[LD,IPD,T(500,500)]];
+    end
+end
+
+pathlengthRatio = pathlengthL(:,3)./pathlengthR(:,3);
+pathlengthDiff = pathlengthL(:,3) - pathlengthR(:,3);
+
+socialEnergyRatio = socialEnergyL(:,3)./socialEnergyR(:,3);
+socialEnergyDiff = socialEnergyL(:,3) - socialEnergyR(:,3);
+
+TimeRatio = TimeL(:,3)./TimeR(:,3);
+TimeDiff = TimeL(:,3) - TimeR(:,3);
+
+realdata = dlmread('./data/finaldata.csv');
+realP = realdata(:,3);
+% scatter(pathlengthRatio,realP)
+% scatter(socialEnergyRatio,realP)
+% scatter(TimeRatio,realP)
+% scatter(pathlengthDiff,realP)
+% scatter(socialEnergyDiff,realP)
+% scatter(TimeDiff,realP)
+% 1./(1+exp(a+b*x))  psychometric
+% 1-exp(-1*(x/a)^b)  weibull
+
+%% Generate Path
+pathall = [];
+for i = 1:5
+    LD = i*0.2 - 0.8;
+    for IPD = 2:4
+        socialImpact = 0;
+        BlockerL = [width/2 + LD , 2.5];
+        BlockerR = [width/2 + LD + IPD , 2.5];
+        EnergyMap1 = GetMap(width/precision+1,Target(2)/precision+1,BlockerL/precision,BlockerL_Ori,m,n,a,b,c,precision);
+        EnergyMap2 = GetMap(width/precision+1,Target(2)/precision+1,BlockerR/precision,BlockerR_Ori,m,n,a,b,c,precision);
+        EnergyMap = EnergyMap1 + EnergyMap2;
+        EnergyMap = rescale(EnergyMap,0,1,'InputMin',0,'InputMax',th);
+        
+        if (realP(3*(i-1)+IPD-1) < 0.5)
+            EnergyMap(round(BlockerL(1)/precision):round(BlockerR(1)/precision),251) = 1;
+        else
+            EnergyMap(1:round(BlockerL(1)/precision),251) = 1;
+            EnergyMap(round(BlockerR(1)/precision:1001),251) = 1;
+        end
+        
+        SpeedMap = 1 - EnergyMap;
+        T = fm(SpeedMap,StartPoint,[1 1],struct('implementation','C++','order',2));
+        path = rayTrace(T,EndPoint,StartPoint,RTsettings);
+        path2 = sortrows(path',2);
+        path3 = interp1(path2(:,2),path2(:,1),1:470);
+        path3(1) = (path3(2)-500)/2+500;
+
+        newpath = [ones(470,1)*LD*100,ones(470,1)*IPD*100,[1:470]',path3'];
+        pathall = [pathall;newpath];
+    end
+end
+pathall(:,4) = pathall(:,4) - 500;
+dlmwrite('./results/path_interaction(social).csv',pathall);
+
+%% Generate Path
+pathall = [];
+for i = 1:5
+    LD = i*0.2 - 0.8;
+    for IPD = 2:4
+        socialImpact = 0;
+        BlockerL = [width/2 + LD , 2.5];
+        BlockerR = [width/2 + LD + IPD , 2.5];
+        EnergyMap1 = GetMap(width/precision+1,Target(2)/precision+1,BlockerL/precision,BlockerL_Ori,m,n,a,b,c,precision);
+        EnergyMap2 = GetMap(width/precision+1,Target(2)/precision+1,BlockerR/precision,BlockerR_Ori,m,n,a,b,c,precision);
+        EnergyMap = EnergyMap1 + EnergyMap2;
+        EnergyMap = rescale(EnergyMap,0,1,'InputMin',0,'InputMax',th);
+        
+        SpeedMap = 1 - EnergyMap;
+        T = fm(SpeedMap,StartPoint,[1 1],struct('implementation','C++','order',2));
+        path = rayTrace(T,EndPoint,StartPoint,RTsettings);
+        path2 = sortrows(path',2);
+        path3 = interp1(path2(:,2),path2(:,1),1:470);
+        path3(1) = (path3(2)-500)/2+500;
+
+        newpath = [ones(470,1)*LD*100,ones(470,1)*IPD*100,[1:470]',path3'];
+        pathall = [pathall;newpath];
+    end
+end
+pathall(:,4) = pathall(:,4) - 500;
+dlmwrite('./results/path_interaction_social.csv',pathall);
---
+++ Computational Modelling/Exp7_Collision.m
@@ -0,0 +1,181 @@
+clc;clear;
+addpath('./functions');
+load('./data/fit_parameters.mat');
+m = x(1);
+n = x(2);
+a = 0.24;
+b = 0.12;
+c = x(3);
+th = x(4)*10;
+r = 5;
+width = 10; % How wide is the map
+EndPoint = [999;500];
+Target = [0,10]; % The position of the target pole
+precision = 0.01; % How precise is the map
+baseSpeed = 1.406; % m/s
+dev_p = 136; % when the avatar starts deviate from midline
+fps = 72; timestep = 1/fps;
+minStep = baseSpeed/precision * timestep; % cm/frame
+RTsettings = struct('useFD',1,'stepSize',1);
+thinkx = (0:minStep:(300+dev_p))';
+thinky = ones(length(thinkx),1)*500;
+think = [thinky,thinkx];
+pathlengthL = [];pathlengthR = [];
+socialEnergyL = [];socialEnergyR = [];
+TimeL = [];TimeR = [];
+subpathall = [];
+
+%% Generate path if MUST left
+for ii = 1:5 % Avatar Start Place
+    for j = 1:3 % Speed
+        socialImpact = 0;
+        deg = 30*ii - 90;
+        speed = (0.2*j + 0.6) * baseSpeed;
+        disp(['Processing LEFT Direction = ' num2str(deg) ', Speed = ' num2str(0.2*j + 0.6) '...']);
+        r1 = r - speed*(dev_p/100/baseSpeed);
+        BlockerStart = [width/2 + r1*sind(deg),r + r1*cosd(deg)];
+        Blocker_Ori = deg * -1;
+        StartPoint = [dev_p;500];
+        
+        timepoints = length(dev_p:minStep:1000);
+        subPath = zeros(timepoints,2);
+        ini_avaPath = zeros(timepoints,2);
+        avastep = [speed*timestep*sind(deg) , speed*timestep*cosd(deg)] .* -1/precision;
+        for time = 1:timepoints
+            subPath(time,:) = [500,dev_p] + [0,minStep]*(time-1);
+            ini_avaPath(time,:) = BlockerStart/precision + avastep*(time-1);
+        end
+        
+        avaPath = ini_avaPath;
+        num = 1;
+        while 1
+            d = subPath - avaPath;
+            d1 = d(:,1).^2 + d(:,2).^2;
+            LD = avaPath(:,2)-subPath(:,2);
+            LD1 = abs(subPath(abs(LD)==min(abs(LD)),1) - avaPath(abs(LD)==min(abs(LD)),1));
+%             disp(['Minimum distance along path: ' num2str(sqrt(min(d1)))]);
+%             disp(['Minimum lateral distance along path: ' num2str(LD1)]);
+%             disp(['Avatar Position: ' num2str(avaPath(d1==min(d1),2))]);
+%             disp(['Subject Position: ' num2str(subPath(d1==min(d1),2))]);
+            if num > 1
+                break
+            end
+            Blocker = avaPath(d1==min(d1),:);
+            EnergyMap = GetPhysicalMap(width/precision+1,Target(2)/precision+1,Blocker,Blocker_Ori,m,n,a,b,c,precision);
+            EnergyMap = rescale(EnergyMap,0,1,'InputMin',0,'InputMax',th);
+            % Key manipulation %
+            EnergyMap(round(Blocker(1)):(width/precision+1),round(Blocker(2))) = 1;
+            % Key manipulation %
+            SpeedMap = 1 - EnergyMap;
+            T = fm(SpeedMap,StartPoint,[1 1],struct('implementation','C++','order',2));
+            path = rayTrace(T,EndPoint,StartPoint,RTsettings);
+            path=[path,flip(StartPoint)];
+            l = arclength(path(1,:),path(2,:));
+            subPath1 = interparc(round(l/minStep)+1,path(1,:),path(2,:));
+            subPath1 = flip(subPath1);
+            subPath = subPath1(1:length(avaPath),:);
+            num = num + 1;
+        end
+        subPath1(:,2) = subPath1(:,2) + 300;
+        subPath1 = flip([think;subPath1],2);
+        subPath1(:,2) = (subPath1(:,2) - 500) * -1;
+        for jj = 1:length(path)-1
+            socialImpact = socialImpact + EnergyMap(round(path(1,j)),round(path(2,j)));
+        end
+        socialEnergyL = [socialEnergyL;socialImpact];
+        pathlengthL = [pathlengthL;arclength(subPath1(:,1),subPath1(:,2))];
+        subpathall = [subpathall;[ones(length(subPath1),1)*ii,ones(length(subPath1),1)*(0.2*j + 0.6),zeros(length(subPath1),1),subPath1]];
+    end
+end
+
+%% Generate path if MUST right
+for ii = 1:5 % Avatar Start Place
+    for j = 1:3 % Speed
+        socialImpact = 0;
+        deg = 30*ii - 90;
+        speed = (0.2*j + 0.6) * baseSpeed;
+        disp(['Processing RIGHT Direction = ' num2str(deg) ', Speed = ' num2str(0.2*j + 0.6) '...']);
+        r1 = r - speed*(dev_p/100/baseSpeed);
+        BlockerStart = [width/2 + r1*sind(deg),r + r1*cosd(deg)];
+        Blocker_Ori = deg * -1;
+        StartPoint = [dev_p;500];
+        
+        timepoints = length(dev_p:minStep:1000);
+        subPath = zeros(timepoints,2);
+        ini_avaPath = zeros(timepoints,2);
+        avastep = [speed*timestep*sind(deg) , speed*timestep*cosd(deg)] .* -1/precision;
+        for time = 1:timepoints
+            subPath(time,:) = [500,dev_p] + [0,minStep]*(time-1);
+            ini_avaPath(time,:) = BlockerStart/precision + avastep*(time-1);
+        end
+        
+        avaPath = ini_avaPath;
+        num = 1;
+        while 1
+            d = subPath - avaPath;
+            d1 = d(:,1).^2 + d(:,2).^2;
+            LD = avaPath(:,2)-subPath(:,2);
+            LD1 = abs(subPath(abs(LD)==min(abs(LD)),1) - avaPath(abs(LD)==min(abs(LD)),1));
+%             disp(['Minimum distance along path: ' num2str(sqrt(min(d1)))]);
+%             disp(['Minimum lateral distance along path: ' num2str(LD1)]);
+%             disp(['Avatar Position: ' num2str(avaPath(d1==min(d1),2))]);
+%             disp(['Subject Position: ' num2str(subPath(d1==min(d1),2))]);
+            if num > 1
+                break
+            end
+            Blocker = avaPath(d1==min(d1),:);
+            EnergyMap = GetPhysicalMap(width/precision+1,Target(2)/precision+1,Blocker,Blocker_Ori,m,n,a,b,c,precision);
+            EnergyMap = rescale(EnergyMap,0,1,'InputMin',0,'InputMax',th);
+            % Key manipulation %
+            EnergyMap(1:round(Blocker(1)),round(Blocker(2))) = 1;
+            % Key manipulation %
+            SpeedMap = 1 - EnergyMap;
+            T = fm(SpeedMap,StartPoint,[1 1],struct('implementation','C++','order',2));
+            path = rayTrace(T,EndPoint,StartPoint,RTsettings);
+            path=[path,flip(StartPoint)];
+            l = arclength(path(1,:),path(2,:));
+            subPath1 = interparc(round(l/minStep)+1,path(1,:),path(2,:));
+            subPath1 = flip(subPath1);
+            subPath = subPath1(1:length(avaPath),:);
+            num = num + 1;
+        end
+        subPath1(:,2) = subPath1(:,2) + 300;
+        subPath1 = flip([think;subPath1],2);
+        subPath1(:,2) = (subPath1(:,2) - 500) * -1;
+        for jj = 1:length(path)-1
+            socialImpact = socialImpact + EnergyMap(round(path(1,j)),round(path(2,j)));
+        end
+        socialEnergyR = [socialEnergyR;socialImpact];
+        pathlengthR = [pathlengthR;arclength(subPath1(:,1),subPath1(:,2))];
+        subpathall = [subpathall;[ones(length(subPath1),1)*ii,ones(length(subPath1),1)*(0.2*j + 0.6),ones(length(subPath1),1),subPath1]];
+    end
+end
+
+socialEnergyRatio = socialEnergyL./socialEnergyR;
+pathlengthRatio = pathlengthL./pathlengthR;
+
+subpathall = subpathall(subpathall(:,4)<1270,:);
+subpathall = [subpathall,zeros(length(subpathall),1)];
+
+x = (1:1270)';
+newsubpathall = [];
+for ii=0:1
+    for j=1:5
+        for k=0.8:0.2:1.2
+            temp = subpathall(subpathall(:,3)==ii & subpathall(:,1)==j & subpathall(:,2)==k,:);
+            y = interp1(temp(:,4),temp(:,5),x,'linear','extrap');
+            newsubpathall = [newsubpathall;[ones(1270,1)*j,ones(1270,1)*k,ones(1270,1)*ii,x,y,zeros(1270,1)]];
+        end
+    end
+end
+newsubpathall = sortrows(newsubpathall,[1 2 3]);
+dlmwrite('./results/path_dynamic(collision).csv',newsubpathall);
+save('./results/dynamicpath(collision).mat','newsubpathall')
+
+realdata = dlmread('./data/final.csv');
+realP = realdata(:,3);
+pathlengthRatio = pathlengthL(:,3)./pathlengthR(:,3);
+socialEnergyRatio = socialEnergyL(:,3)./socialEnergyR(:,3);
+% 1./(1+exp(a+b*x))
+modelPrediction = [realP,socialEnergyRatio];
+dlmwrite('./results/corD_c.csv',modelPrediction);
